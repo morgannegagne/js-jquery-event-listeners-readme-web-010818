effbb616cd3de5d5e739654cfc9c6889d329c290
morgannegagne <morgannegagne@gmail.com>
2018-02-27 16:40:05 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
 
+function getIt(){
+  $('p').on("click", event => alert("Hey!"))
+}
+function frameIt(){
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+function pressIt(){
+  $('#typing').on('keydown', function(event){
+    console.log(event)
+    if (event.which === 71){
+      alert('You pressed the G key!')
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+
 $(document).ready(function(){
 
 // call functions here
-
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
